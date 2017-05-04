32cc9848bba825a70ed02c5069c073edcbf7909b
Kevin Wang <kvin.w2@gmail.com>
2017-05-04 18:57:44 -0400

    better error handling

--- HelpBot/MessageParser.py
+++ HelpBot/MessageParser.py
@@ -1,5 +1,6 @@
 import re
 from dateutil import parser
+from datetime import datetime
 
 
 class BadMessage(Exception):
@@ -13,13 +14,21 @@ class MessageParser:
     def parse_directions(self, string):
         regex = "^(\w+) directions from (.+) to (.+) (depart at|arrive by) ([^ ]*)$"
         match_obj = re.match(regex, string)
+        time_str = match_obj.group(5)
+        if isinstance(time_str, str) and time_str.lower() == "now":
+            time = datetime.now()
+        else:
+            try:
+                time = parser.parse(time_str)
+            except:
+                raise BadMessage("Invalid time input")
         try:
             return {
                 'mode': match_obj.group(1),
                 'origin': match_obj.group(2),
                 'destination': match_obj.group(3),
                 'time_modifier': match_obj.group(4),
-                'time': parser.parse(match_obj.group(5))
+                'time': time
             }
         except:
-            raise BadMessage()
+            raise BadMessage("Must follow message format")
